e434780366e8d1152806461ee25fe5e30055c600
Arjun Prem <arjundprem@outlook.com>
2023-05-29 20:34:28 -0700

    Shuffling a sequence of numbers in an optimized way

---
+++ c++/random_number_shuffling/random_number_shuffling/random_number_shuffling.cpp
@@ -0,0 +1,34 @@
+﻿// random_number_shuffling.cpp : Defines the entry point for the application.
+//
+#include <iostream>
+#include <vector>
+#include <vector>
+#include <numeric>
+
+
+class Shuffle {
+public:
+	Shuffle(std::vector<int> _v) : v(_v) {};
+	int pop() {
+		std::swap(v[rand() % v.size()], v[v.size() - 1]);
+		int num = v.back();
+		v.pop_back();
+		return num;
+	}
+	bool isNumLeft() {
+		return v.size() > 0;
+	}
+private:
+	std::vector<int> v;
+};
+
+
+int main() {
+	std::vector<int> v(10);
+	std::iota(v.begin(), v.end(), 0);
+	Shuffle shuffle(v);
+	while (shuffle.isNumLeft()) {
+		std::cout << "Number: " << shuffle.pop() << "\n";
+	}
+	return 0;
+}
